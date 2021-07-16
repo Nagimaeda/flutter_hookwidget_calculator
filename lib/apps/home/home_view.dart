8374927f1c2f7ace94ea70987d147bca7113ceb2
Jerome Estrella <jerome.estrella15@gmail.com>
2021-07-16 20:40:55 +0800

    New calculator
    Less code clutter, converted to HookWidget

--- lib/apps/home/home_view.dart
+++ lib/apps/home/home_view.dart
@@ -1,333 +1,126 @@
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
-
+import 'buttons.dart';
 import 'home_view_model.dart';
 
 T useChangeNotifierListenable<T extends ChangeNotifier>(T data){
   final state = useState<T>(data);
   return useListenable(state.value);
 }
+// Array of button
+final List<String> buttons = [
+  'C',
+  '+/-',
+  '%',
+  'DEL',
+  '7',
+  '8',
+  '9',
+  '/',
+  '4',
+  '5',
+  '6',
+  'x',
+  '1',
+  '2',
+  '3',
+  '-',
+  '0',
+  '.',
+  '=',
+  '+',
+];
 
 class HomeView extends HookWidget{
-
   @override
   Widget build(BuildContext context) {
     final homeChangeNotifier = useChangeNotifierListenable(HomeChangeNotifier());
     return Scaffold(
-      appBar: AppBar(
-
-        title: Text("Calculator"),
+      appBar: new AppBar(
+        title: new Text("Calculator"),
       ),
-      body:
-      Column(
-        children:[
-          Row(
-            children:[
-            Expanded(
-              child:
-              Container(
-                color: Colors.grey,
-                alignment: Alignment.bottomRight,
-                child: Text("${homeChangeNotifier.display}"),
-                height: 70,
-              ),
+      backgroundColor: Colors.white38,
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: Container(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Container(
+                      padding: EdgeInsets.all(20),
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        "${homeChangeNotifier.display}",
+                        style: TextStyle(fontSize: 18, color: Colors.white),
+                      ),
+                    ),
+                    Container(
+                      padding: EdgeInsets.all(15),
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        "${homeChangeNotifier.answer}",
+                        style: TextStyle(
+                            fontSize: 30,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    )
+                  ]),
             ),
-            ]
           ),
           Expanded(
-          child:
-        Table(
-          border: TableBorder.all(),
-          columnWidths: const <int, TableColumnWidth>{
-            0: FlexColumnWidth(),
-            1: FlexColumnWidth(),
-            2: FlexColumnWidth(),
-            3: FlexColumnWidth(),
-          },
-          defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-          children: <TableRow>[
-            TableRow(
-              children: <Widget>[
-                TableCell(
-                  verticalAlignment: TableCellVerticalAlignment.middle,
-                  child: InkWell(
-                    onTap: (){_buttonPressed(homeChangeNotifier, "C");},
-                    child:
-                      Container(
-                        height: 64,
-                        alignment: Alignment.center,
-                        child: Text("C"),
-                  ),
-                ),
-                ),
-                TableCell(
-                  verticalAlignment: TableCellVerticalAlignment.middle,
-                child: InkWell(
-                  onTap: (){_buttonPressed(homeChangeNotifier, "+/-");},
-                  child: Container(
-                    height: 64,
-                    alignment: Alignment.center,
-                    child: Text("+/-"),
-                  ),
-                ),
-                ),
-                TableCell(
-                  verticalAlignment: TableCellVerticalAlignment.middle,
-                  child: InkWell(
-                    onTap: (){_buttonPressed(homeChangeNotifier, "%");},
-                    child: Container(
-                      height: 64,
-                      alignment: Alignment.center,
-                      child: Text("%"),
-                  ),
-                ),
-                ),
-                TableCell(
-                  verticalAlignment: TableCellVerticalAlignment.middle,
-                  child: InkWell(
-                    onTap: (){_buttonPressed(homeChangeNotifier, "DEL");},
-                    child: Container(
-                      height: 64,
-                      alignment: Alignment.center,
-                      child: Text("DEL"),
-                  ),
-                ),
-                ),
-              ],
+            flex: 3,
+            child: Container(
+              child: GridView.builder(
+                  itemCount: buttons.length,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 4),
+                  itemBuilder: (BuildContext context, int index) {
+                    //equal button
+                    if (index == 18) {
+                      return MyButton(
+                        buttontapped: () {
+                          _buttonPressed(homeChangeNotifier, buttons[index]);
+                        },
+                        buttonText: buttons[index],
+                        color: Colors.orange[700],
+                        textColor: Colors.white,
+                      );
+                    }
+
+                    //  other buttons
+                    else {
+                      return MyButton(
+                        buttontapped: () {
+                          _buttonPressed(homeChangeNotifier, buttons[index]);
+                        },
+                        buttonText: buttons[index],
+                        color: isOperator(buttons[index])
+                            ? Colors.blueAccent
+                            : Colors.white,
+                        textColor: isOperator(buttons[index])
+                            ? Colors.white
+                            : Colors.black,
+                      );
+                    }
+                  }), // GridView.builder
             ),
-            TableRow(
-              children: <Widget>[
-                TableCell(
-                  verticalAlignment: TableCellVerticalAlignment.middle,
-                  child: InkWell(
-                    onTap: (){_buttonPressed(homeChangeNotifier, "7");},
-                    child: Container(
-                      height: 64,
-                      alignment: Alignment.center,
-                      child: Text("7"),
-                    ),
-                ),
-                ),
-                TableCell(
-                  verticalAlignment: TableCellVerticalAlignment.middle,
-                  child: InkWell(
-                    onTap: (){_buttonPressed(homeChangeNotifier, "8");},
-                    child: Container(
-                      height: 64,
-                      alignment: Alignment.center,
-                      child: Text("8"),
-                    ),
-                ),
-                ),
-                TableCell(
-                  verticalAlignment: TableCellVerticalAlignment.middle,
-                  child: InkWell(
-                    onTap: (){_buttonPressed(homeChangeNotifier, "9");},
-                    child: Container(
-                      height: 64,
-                      alignment: Alignment.center,
-                      child: Text("9"),
-                   ),
-                ),
-                ),
-                TableCell(
-                  verticalAlignment: TableCellVerticalAlignment.middle,
-                  child: InkWell(
-                    onTap: (){_buttonPressed(homeChangeNotifier, "/");},
-                    child: Container(
-                      color: Colors.amber,
-                      height: 64,
-                      alignment: Alignment.center,
-                      child: Text("/"),
-                    ),
-                  ),
-                ),
-              ],
-            ),
-            TableRow(
-              children: <Widget>[
-                TableCell(
-                  verticalAlignment: TableCellVerticalAlignment.middle,
-                  child: InkWell(
-                    onTap: (){_buttonPressed(homeChangeNotifier, "4");},
-                    child: Container(
-                      height: 64,
-                      alignment: Alignment.center,
-                      child: Text("4"),
-                    ),
-                ),
-                ),
-                TableCell(
-                  verticalAlignment: TableCellVerticalAlignment.middle,
-                  child: InkWell(
-                  onTap: (){_buttonPressed(homeChangeNotifier, "5");},
-                  child:
-                    Container(
-                      height: 64,
-                      alignment: Alignment.center,
-                      child: Text("5"),
-                    ),
-                ),
-                ),
-                TableCell(
-                  verticalAlignment: TableCellVerticalAlignment.middle,
-                  child: InkWell(
-                  onTap: (){_buttonPressed(homeChangeNotifier, "6");},
-                  child:
-                    Container(
-                      height: 64,
-                      alignment: Alignment.center,
-                      child: Text("6"),
-                    ),
-                ),
-                ),
-                TableCell(
-                  verticalAlignment: TableCellVerticalAlignment.middle,
-                  child: InkWell(
-                  onTap: (){_buttonPressed(homeChangeNotifier, "X");},
-                  child:
-                    Container(
-                      color: Colors.amber,
-                      height: 64,
-                      alignment: Alignment.center,
-                      child: Text("X"),
-                    ),
-                ),
-                ),
-              ],
-            ),
-            TableRow(
-              children: <Widget>[
-                TableCell(
-                  verticalAlignment: TableCellVerticalAlignment.middle,
-                  child: InkWell(
-                    onTap: (){_buttonPressed(homeChangeNotifier, "1");},
-                    child:
-                    Container(
-                      height: 64,
-                      alignment: Alignment.center,
-                      child: Text("1"),
-                    ),
-                  ),
-                ),
-                TableCell(
-                  verticalAlignment: TableCellVerticalAlignment.middle,
-                child: InkWell(
-                  onTap: (){_buttonPressed(homeChangeNotifier, "2");},
-                  child:
-                    Container(
-                      height: 64,
-                      alignment: Alignment.center,
-                      child: Text("2"),
-                    ),
-                ),
-                ),
-                TableCell(
-                  verticalAlignment: TableCellVerticalAlignment.middle,
-                child: InkWell(
-                  onTap: (){_buttonPressed(homeChangeNotifier, "3");},
-                  child:
-                    Container(
-                      height: 64,
-                      alignment: Alignment.center,
-                      child: Text("3"),
-                    ),
-                ),
-                ),
-                TableCell(
-                  verticalAlignment: TableCellVerticalAlignment.middle,
-                child: InkWell(
-                  onTap: (){_buttonPressed(homeChangeNotifier, "-");},
-                  child:
-                    Container(
-                      color: Colors.amber,
-                      height: 64,
-                      alignment: Alignment.center,
-                      child: Text("-"),
-                    ),
-                ),
-                ),
-              ],
-            ),
-            TableRow(
-              children: <Widget>[
-                TableCell(
-                  verticalAlignment: TableCellVerticalAlignment.middle,
-                  child: InkWell(
-                    onTap: (){_buttonPressed(homeChangeNotifier, "0");},
-                    child:
-                    Container(
-                      height: 64,
-                      alignment: Alignment.center,
-                      child: Text("0"),
-                    ),
-                ),
-                ),
-                TableCell(
-                  verticalAlignment: TableCellVerticalAlignment.middle,
-                  child: InkWell(
-                    onTap: (){_buttonPressed(homeChangeNotifier, ".");},
-                    child:
-                      Container(
-                        height: 64,
-                        alignment: Alignment.center,
-                        child: Text("."),
-                      ),
-                ),
-                ),
-                TableCell(
-                  verticalAlignment: TableCellVerticalAlignment.middle,
-                  child: InkWell(
-                    onTap: (){_buttonPressed(homeChangeNotifier, "=");},
-                    child:
-                    Container(
-                      color: Colors.amber,
-                      height: 64,
-                      alignment: Alignment.center,
-                      child: Text("="),
-                    ),
-                ),
-                ),
-                TableCell(
-                  verticalAlignment: TableCellVerticalAlignment.middle,
-                  child: InkWell(
-                    onTap: (){_buttonPressed(homeChangeNotifier, "+");},
-                    child:
-                      Container(
-                        color: Colors.amber,
-                        height: 64,
-                        alignment: Alignment.center,
-                        child: Text("+"),
-                      ),
-                ),
-                ),
-              ],
-            ),
-    ],
-      ),
-        ),
-    ],
+          ),
+        ],
       ),
     );
   }
 }
-
-  Future<void> _buttonPressed(HomeChangeNotifier homeChangeNotifier, String pressed) async{
-    homeChangeNotifier.reloadState(pressed);
+  bool isOperator(String x) {
+  if (x == '/' || x == 'x' || x == '-' || x == '+' || x == '=') {
+  return true;
+  }
+  return false;
   }
 
-// Text(
-//  'You have pushed the button this many times:',
-// ),
-// Text(
-//  '${homeChangeNotifier.increment}',
-//  style: Theme.of(context).textTheme.headline4,
-// ),
-// floatingActionButton: FloatingActionButton(
-//         onPressed: () async{
-//           _incrementCounter(homeChangeNotifier);
-//         },
-//         tooltip: 'Increment',
-//         child: Icon(Icons.add),
-//       ),
+  Future<void> _buttonPressed(HomeChangeNotifier homeChangeNotifier, String pressed) async{
+  print("button pressed");
+    homeChangeNotifier.reloadState(pressed);
+  }
--- lib/apps/home/home_view_model.dart
+++ lib/apps/home/home_view_model.dart
@@ -1,119 +1,57 @@
 import 'package:flutter/cupertino.dart';
-
+import 'package:math_expressions/math_expressions.dart';
 class HomeChangeNotifier extends ChangeNotifier {
-  String display = "0";
-  var operation = null;
-  bool firstPress = true;
-  double firstValue = 0;
+  String display = "";
+  String answer = "0";
+  void equalPressed() {
+    String finaluserinput = display;
+    finaluserinput = display.replaceAll('x', '*');
 
+    Parser p = Parser();
+    Expression exp = p.parse(finaluserinput);
+    ContextModel cm = ContextModel();
+    double eval = exp.evaluate(EvaluationType.REAL, cm);
+    answer = eval.toString();
+  }
   Future<void> reloadState(String pressed) async {
-    double dbl(String x){
+    double dbl(String x) {
       return double.parse(x);
     }
-    String dblString(String x){
-      return double.parse(x).toString();
-    }
-
-    if (operation == null){
-
-      switch(pressed){
-      case "C": {
-        firstValue = 0;
-        display = "0";
-      }break;
-      case "+/-":{
-        display =  (-1 * dbl(display)).toString();
-      }break;
-      case "%":{
-        display = (dbl(display)/100).toString();
-      }break;
-      case "DEL":{
-        display = display.substring(0,display.length-1);
-      }break;
-      case "/":{
-        operation = "/";
-        firstValue = dbl(display);
-      } break;
-      case "+":{
-        operation = "+";
-        firstValue = dbl(display);
-      } break;
-      case "-":{
-        operation = "-";
-        firstValue = dbl(display);
-      } break;
-      case "X":{
-        operation = "X";
-        firstValue = dbl(display);
-      } break;
-        case "=":{
-          print ("wala namang operation hatdog");
-        }break;
-      default: {
-        display = display+pressed;
-      }
-    }
-    }else{
-      switch(pressed) {
-        case "C":{
-            firstValue = 0;
-            display = "0";
-            operation = null;
-          }break;
-        case "+/-":{
-          if (firstPress){}else {
-            display = (-1 * dbl(display)).toString();
-          }
-        }break;
-        case "%":{
-          if(firstPress){}else {
-            display = (dbl(display) / 100).toString();
-          }
-        }break;
-        case "DEL":{
-          if(firstPress){}else {
-            display = display.substring(0, display.length - 1);
-          }
-        }break;
-        case "/":{
-          print("Press C to clear and change operations.");
-        } break;
-        case "+":{
-          print("Press C to clear and change operations.");
-        } break;
-        case "-":{
-          print("Press C to clear and change operations.");
-        } break;
-        case "X":{
-          print("Press C to clear and change operations.");
-        } break;
-        case "=":{
-          switch(operation){
-            case "+":{
-              display = (firstValue + dbl(display)).toString();
-            }break;
-            case "-":{
-              display = (firstValue - dbl(display)).toString();
-            }break;
-            case "X":{
-              display = (firstValue * dbl(display)).toString();
-            }break;
-            case "/":{
-              display = (firstValue / dbl(display)).toString();
-            }break;
-          }
-          firstPress = true;
-          operation = null;
-        }break;
-        default: {
-          if(firstPress){
-            display = "";
-          }
-          display = display+pressed;
-          firstPress = false;
+    switch (pressed) {
+      case "C":
+        {
+          display = "";
+          answer = "0";
         }
-      }
+        break;
+      case "+/-":
+        {
+          print("why so negative");
+          try{
+            //if possible, negate the current value; if not, just add a negative sign to the input
+          display = (-1 * dbl(display)).toString();}catch(Exception){display+="-";}
+        }
+        break;
+      case "%":
+        {
+            display+="%";
+        }
+        break;
+      case "DEL":
+        {
+          display = display.substring(0, display.length - 1);
+        }
+        break;
+      case "=":
+        {
+          equalPressed();
+        }
+        break;
+      default:
+        {
+          display = display + pressed;
+        }
     }
-  notifyListeners();
+    notifyListeners();
   }
 }
